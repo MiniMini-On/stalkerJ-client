3ad9c47d31bb200f47009fc2b30c8bdd7619ee70
MiniMini-On <120891914+MiniMini-On@users.noreply.github.com>
2023-02-14 23:46:34 +0900

    Update KakaoShareBtn.js

--- src/components/KakaoShareBtn.js
+++ src/components/KakaoShareBtn.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styles from "./KakaoShareBtn.module.css";
 function KakaoShareBtn() {
   useEffect(() => {
-    window.Kakao.init(process.env.REACT_APP_JAVASCRIPT_KEY);
+    window.Kakao.init(6aa08bc3745f3e2c3f580e31c3822e5d);
     window.Kakao.isInitialized(); // init되면 true, 아니면 false를 반환한다
   }, []);
   const shareKakao = () => {
