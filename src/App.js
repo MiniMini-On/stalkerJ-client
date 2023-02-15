7479ad6a41dfe1256c89be789a6244f97d875412
MiniMini-On <parkcy92@gmail.com>
2023-02-16 00:30:09 +0900

    :art: edit
    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     setTimeout(function () {
       setInit(1);
-    }, 800);
+    }, 900);
   }, []);
   return (
     <div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
   useEffect(() => {
     setTimeout(function () {
       setInit(1);
-    }, 800);
+    }, 900);
   }, []);
 
   useEffect(() => {
